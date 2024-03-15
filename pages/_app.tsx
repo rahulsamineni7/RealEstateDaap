e3bff4e28ab3e1030a5206360388b0a0e6bbffaa
rahulsamineni <rahulsamineni1234@gmail.com>
2024-03-15 20:04:04 +0530

    first commit

---
+++ pages/_app.tsx
@@ -0,0 +1,25 @@
+import type { AppProps } from "next/app";
+import { ThirdwebProvider } from "@thirdweb-dev/react";
+import { Navbar } from "../components/Navbar/Navbar";
+
+import { NETWORK } from "../const/contractAddresses";
+import "../styles/globals.css";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ThirdwebProvider
+      clientId={process.env.NEXT_PUBLIC_TEMPLATE_CLIENT_ID}
+      activeChain={NETWORK}
+    >
+      {/* Progress bar when navigating between pages */}
+     
+
+      {/* Render the navigation menu above each component */}
+      <Navbar />
+      {/* Render the actual component (page) */}
+      <Component {...pageProps} />
+    </ThirdwebProvider>
+  );
+}
+
+export default MyApp;
